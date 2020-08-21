7b050a41405ad27bec8caefd8481718248412a7a
nbehmane <nbehmane@calpoly.edu>
2020-08-20 21:36:15 -0700

    Changed Flag stuff...

--- src/Flag.java
+++ src/Flag.java
@@ -19,71 +19,45 @@ public class Flag {
                 imageStore.getImageList(color2));
         Background red = new Background(color3,
                 imageStore.getImageList(color3));
-
         Point point = new Point(pos.x, pos.y);
         for (int i = 0; i < 15; i++)
         {
             world.removeEntityAt(point);
-
             world.removeEntityAt(
-                    new Point(point.x, point.y + 1)
-            );
+                    new Point(point.x, point.y + 1));
             world.removeEntityAt(
-                    new Point(point.x, point.y + 2)
-            );
+                    new Point(point.x, point.y + 2));
             world.removeEntityAt(
-                    new Point(point.x, point.y + 3)
-            );
-
+                    new Point(point.x, point.y + 3));
             world.removeEntityAt(
-                    new Point(point.x, point.y + 4)
-            );
-
+                    new Point(point.x, point.y + 4));
             world.removeEntityAt(
-                    new Point(point.x, point.y + 5)
-            );
+                    new Point(point.x, point.y + 5));
             world.removeEntityAt(
-                    new Point(point.x, point.y + 6)
-            );
+                    new Point(point.x, point.y + 6));
             world.removeEntityAt(
-                    new Point(point.x, point.y + 7)
-            );
+                    new Point(point.x, point.y + 7));
             world.removeEntityAt(
-                    new Point(point.x, point.y + 8)
-            );
+                    new Point(point.x, point.y + 8));
             point.x += 1;
-        }
-        for (int i = 0; i < 15; i++)
-        {
-
             green.setBackground(world, pos);
-
             green.setBackground(world,
                     new Point(pos.x, pos.y + 1));
-
             green.setBackground(world,
                     new Point(pos.x, pos.y + 2));
-
             white.setBackground(world,
                     new Point(pos.x, pos.y + 3));
-
             white.setBackground(world,
                     new Point(pos.x, pos.y + 4));
-
             white.setBackground(world,
                     new Point(pos.x, pos.y + 5));
-
             red.setBackground(world,
                     new Point(pos.x, pos.y + 6));
-
             red.setBackground(world,
                     new Point(pos.x, pos.y + 7));
-
             red.setBackground(world,
                     new Point(pos.x, pos.y + 8));
             pos.x += 1;
         }
     }
-
-
 }
